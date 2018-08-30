d41e8cb9f836d409454d88e19094985cb9609f4c
allievo04 ires <allievo04.ires@outlook.it>
2018-08-30 12:32:25 +0200

    main.js changed

--- wwwroot/scripts/logic/turn-mechanics.js
+++ wwwroot/scripts/logic/turn-mechanics.js
@@ -1,4 +1,5 @@
 import { setBoardCellValue, getBoardLength, getBoardCellValue, isGameFinished} from "../board/board.js"
+import { goGame} from "../main.js"
 
 var currentPlayer = [];
 var players = [];
@@ -15,6 +16,7 @@ function startGame(playerCollection) {
 
     if(endGame){
         alert(`Congrats Player '${currentPlayer.color}`);
+        goGame();
         }
 }
 
