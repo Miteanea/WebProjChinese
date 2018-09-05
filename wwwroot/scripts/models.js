b73fdf51e63aee1099edb9df8236b14dbec0bc60
Dmitri Caragheaur <dmitri@caragheaur@gmail.com>
2018-09-05 12:02:40 +0200

    Piece Refactored As Class

--- wwwroot/scripts/models.js
+++ wwwroot/scripts/models.js
@@ -22,40 +22,6 @@ class Player {
             }
         }
     }
-    makeMove() {
-
-        var piecesToMove = []
-
-        for (let piece of this.pieceCollection) {
-            var possmov = piece.getPossibleMoves();
-            if (possmov != null) {
-                piecesToMove.push(piece);
-            }
-        }
-
-        var selectedPiece = (piecesToMove[Math.floor(Math.random() * piecesToMove.length)]);
-        selectedPiece.element.trigger("mouseover");
-        console.log(selectedPiece)
-
-        var paths = selectedPiece.getPaths();
-        console.log("Paths:");
-        console.log(paths)
-        var selectedPath = (paths[Math.floor(Math.random() * paths.length)]);
-        console.log("selectedPath: ")
-        console.log(selectedPath);
-
-        //
-        $(this).addClass("soldierSelected");
-        var coord = getCoord(selectedPiece.circle[0].id);
-        setBoardCellValue(coord, "e");
-        var element = document.getElementById(selectedPiece.element[0].id);
-        coord = getCoord(`${selectedPath[0].i}.${selectedPath[0].j}`);
-        setBoardCellValue(coord, element.id.charAt(0));
-        document.getElementById(`${selectedPath[0].i}.${selectedPath[0].j}`).appendChild(element);
-        $("*").removeClass("circleHighlighted");
-        $("*").removeClass("soldierSelected")
-
-    }
     createTargetCell(playerColor) {
         var targetCell;
         switch (playerColor) {
@@ -71,26 +37,22 @@ class Player {
     }
     toggleCurrentPlayer(playerColor) {
         var color = playerColor;
-        $(`.soldier[id^='${color}']`).toggleClass("soldierCurrent");        
+        $(`.soldier[id^='${color}']`).toggleClass("soldierCurrent");
     }
 }
 
-function Piece(color, i, j) {
-    this.color = color;
-    this.i = i;
-    this.j = j;
-    this.element = $(`#${color}${this.i}\\.${this.j}`);
-    this.circle = $(`#${color}${i}\\.${j}`).parent();
-    this.id = `${color}${this.i}.${this.j}`;
-    this.moved = false;
-    this.move = { from: { i: this.i, j: this.j }, to: { i: null, j: null } }
+class Piece {
+    constructor(color, i, j) {
+        this.color = color;
+        this.i = i;
+        this.j = j;
+        this.element = $(`#${color}${this.i}\\.${this.j}`);
+        this.circle = $(`#${color}${i}\\.${j}`).parent();
+        this.id = `${color}${this.i}.${this.j}`;
+        this.moved = false;
+        this.move = { from: new Coord(this.i, this.j), to: new Coord(null, null) }
+    }
 }
-Piece.prototype.getPossibleMoves = function () {
-    this.element.trigger("mouseover");
-    var possmov = getPossibleMovesMain();
-    this.element.trigger("mouseleave");
-    return possmov;
-};
 
 function Coord(i, j) {
     this.i = i;
