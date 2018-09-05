5ba2107674b3b9a1f6c1ff96fa14f253c0506b86
Dmitri Caragheaur <dmitri@caragheaur@gmail.com>
2018-09-05 11:55:56 +0200

    Player Refactored

--- wwwroot/scripts/models.js
+++ wwwroot/scripts/models.js
@@ -2,15 +2,14 @@
 import { getCoord } from "./main.js"
 import { setBoardCellValue, getBoardLength, getBoardCellValue } from "./board/board.js"
 
-<<<<<<< HEAD
 class Player {
-    constructor(playerColor){
+    constructor(playerColor) {
         this.color = playerColor;
         this.pieceCollection = this.getPieces(this.color);
-        this.targetCell = this.createTargetCell(this.color);    
+        this.targetCell = this.createTargetCell(this.color);
     }
-    
-    getPieces = function (color) {
+
+    getPieces(color) {
         var collection = [];
         for (var i = 0; i < getBoardLength(); i++) {
             for (var j = 0; j < getBoardLength(); j++) {
@@ -23,28 +22,28 @@ class Player {
             }
         }
     }
-    makeMove = function () {
-    
+    makeMove() {
+
         var piecesToMove = []
-    
+
         for (let piece of this.pieceCollection) {
             var possmov = piece.getPossibleMoves();
             if (possmov != null) {
                 piecesToMove.push(piece);
             }
         }
-    
+
         var selectedPiece = (piecesToMove[Math.floor(Math.random() * piecesToMove.length)]);
         selectedPiece.element.trigger("mouseover");
         console.log(selectedPiece)
-    
+
         var paths = selectedPiece.getPaths();
         console.log("Paths:");
         console.log(paths)
         var selectedPath = (paths[Math.floor(Math.random() * paths.length)]);
         console.log("selectedPath: ")
         console.log(selectedPath);
-    
+
         //
         $(this).addClass("soldierSelected");
         var coord = getCoord(selectedPiece.circle[0].id);
@@ -55,56 +54,24 @@ class Player {
         document.getElementById(`${selectedPath[0].i}.${selectedPath[0].j}`).appendChild(element);
         $("*").removeClass("circleHighlighted");
         $("*").removeClass("soldierSelected")
-    
+
     }
-    createTargetCell = function (playerColor) {
+    createTargetCell(playerColor) {
         var targetCell;
         switch (playerColor) {
-    
-            case "r": targetCell = { i: 16, j: 4, id: "16.4" };break;
-            case "y": targetCell = { i: 12, j: 0, id: "12.0" };break;
+
+            case "r": targetCell = { i: 16, j: 4, id: "16.4" }; break;
+            case "y": targetCell = { i: 12, j: 0, id: "12.0" }; break;
             case "B": targetCell = { i: 4, j: 4, id: "4.4" }; break;
             case "w": targetCell = { i: 0, j: 12, id: "0.12" }; break;
             case "g": targetCell = { i: 4, j: 16, id: "4.16" }; break;
-            case "b": targetCell = { i: 12, j: 12, id: "12.12"}; break;
+            case "b": targetCell = { i: 12, j: 12, id: "12.12" }; break;
         }
         return targetCell;
     }
-    toggleCurrentPlayer = function (playerColor) {
+    toggleCurrentPlayer(playerColor) {
         var color = playerColor;
-        $(`.soldier[id^='${color}']`).toggleClass("soldierCurrent");
-=======
-function Player(playerColor) {
-    this.color = playerColor;
-    this.pieceCollection = this.getPieces(this.color);
-    this.targetCell = this.createTargetCell(this.color);
-}
-Player.prototype.getPieces = function (color) {
-    var collection = [];
-    for (var i = 0; i < getBoardLength(); i++) {
-        for (var j = 0; j < getBoardLength(); j++) {
-            var coord = new Coord(i,j);
-            if (getBoardCellValue(coord) == color) {
-                var soldier = new Piece(color, i, j);
-                collection.push(soldier);
-                if (collection.length == 10) { return collection; }
-            }
-        }
-    }
-}
-Player.prototype.makeMove = function () {
-}
-Player.prototype.createTargetCell = function (playerColor) {
-    var targetCell;
-    switch (playerColor) {
-
-        case "r": targetCell = { i: 16, j: 4, id: "16.4" };break;
-        case "y": targetCell = { i: 12, j: 0, id: "12.0" };break;
-        case "B": targetCell = { i: 4, j: 4, id: "4.4" }; break;
-        case "w": targetCell = { i: 0, j: 12, id: "0.12" }; break;
-        case "g": targetCell = { i: 4, j: 16, id: "4.16" }; break;
-        case "b": targetCell = { i: 12, j: 12, id: "12.12"}; break;
->>>>>>> 295494f7fdbd1e01da3ac6e2e582792242650a7e
+        $(`.soldier[id^='${color}']`).toggleClass("soldierCurrent");        
     }
 }
 
